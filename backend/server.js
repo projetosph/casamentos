d4bf1b58e6b7cd32fb13af2ebe450c90ab03f1d6
Paulo Henrique Nascimento Conceicao Junior <t863572@embasanet.ba.gov.br>
2026-03-27 12:44:17 -0300

    fix servidor render

--- backend/server.js
+++ backend/server.js
@@ -6,29 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// TESTE
+// ROTA TESTE
 app.get("/", (req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
-// ROTA DE TESTE DE PAGAMENTO (SEM MERCADO PAGO AINDA)
-app.post("/criar-pagamento", (req, res) => {
-  console.log("Recebido:", req.body);
-
-  res.json({
-    link: "https://www.mercadopago.com.br"
-  });
-});
-
-// WEBHOOK
-app.post("/webhook", (req, res) => {
-  console.log("Webhook recebido");
-  res.sendStatus(200);
-});
-
-// PORTA (ESSENCIAL PRO RENDER)
+// IMPORTANTE: PORTA DO RENDER
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Rodando na porta " + PORT);
+  console.log("Servidor rodando na porta " + PORT);
 });
